a66905520c6e7eb8e9061651381bfc25c734241e
Медведев Ваня <ranzar2000@gmail.com>
2024-10-14 04:53:02 +0300

    Implement the useObserver hook

---
+++ src/hooks/useObserver.ts
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+
+/**
+ * Создает IntersectionObserver, который вызывает callback,
+ * когда элемент пересекается с viewport.
+ *
+ * @param {React.MutableRefObject<HTMLElement | null>} ref - Элемент, который
+ *   будет отслеживаться
+ * @param {boolean} canLoad - Может ли observer вызывать callback
+ * @param {boolean} isLoading - статус загрузки
+ * @param {() => void | Promise<void>} callback - Callback, который будет вызван,
+ *   когда элемент пересечется с наблюдаемым элементом
+ */
+export const useObserver = (
+  ref: React.MutableRefObject<HTMLElement | null>,
+  canLoad: boolean,
+  isLoading: boolean,
+  callback: () => void | Promise<void>
+): void => {
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (observer.current) observer.current.disconnect();
+
+    const cb = (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting && canLoad) {
+        callback();
+      }
+    };
+    observer.current = new IntersectionObserver(cb);
+    observer.current.observe(ref.current as HTMLElement);
+  }, [callback, canLoad, isLoading, ref]);
+};
