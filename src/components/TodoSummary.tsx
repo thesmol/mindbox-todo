b6a9077c589b3f51e4d1c0989b089eed55194003
Медведев Ваня <ranzar2000@gmail.com>
2024-10-14 03:21:52 +0300

    Start developing of the TodoSummary component

---
+++ src/components/TodoSummary.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+const TodoSummary = () => {
+  return (
+    <Box className="relative w-full paper p-4">
+      <p>Контент вашего контейнера здесь</p>
+    </Box>
+  );
+};
+
+export default TodoSummary;
