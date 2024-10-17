d18393e365619eaa1de3d1290418371a9e20a9a4
Медведев Ваня <ranzar2000@gmail.com>
2024-10-18 01:26:49 +0300

    Compile all component together

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,68 +1,40 @@
 import { Box } from "@mui/material";
-import { Todo } from "../types.ts/todo";
-import TodoList from "../components/TodoList";
+
+import TodoForm from "../components/TodoForm";
 import CarActivity from "../components/ui/CarActivity";
-import { useCallback, useEffect, useRef, useState } from "react";
-import { useFetching } from "../hooks/useFetching";
-import TodoService from "../api/TodoService";
-import { getBatchCount } from "../utils/getBatchCount";
-import { useObserver } from "../hooks/useObserver";
 
 const TodoPage = () => {
-  /** Список тудушек */
-  const [todos, setTodos] = useState<Todo[]>([]);
-  /** Текущий набор загруженных тудушек (по умолчанию 1) */
-  const [batch, setBatch] = useState<number>(1);
-  /** Общее количество наборов загруженных данных */
-  const [totalBatches, setTotalBatches] = useState(0);
-  /** реф на последний видимый элемент, который триггерит подгрузку новых элементов */
-  const lastElement = useRef<HTMLDivElement | null>(null);
-
-  /** Лимит загрузки тудушек (по умолчанию 10) */
-  const limit = 10;
-
-  /** Мемоизируем fetchTodos с использованием useCallback */
-  const [fetchTodos, isTodosLoading, todoError] = useFetching(
-    useCallback(async (limit: number, page: number) => {
-      const response = await TodoService.getAll(limit, page);
-      setTodos((prevTodos) => [...prevTodos, ...response.data]);
-      const totalCount = response.headers["x-total-count"];
-      setTotalBatches(getBatchCount(totalCount, limit));
-    }, [])
-  );
-
-  useObserver(lastElement, batch < totalBatches, isTodosLoading, () => {
-    setBatch(batch + 1);
-  });
-
-  useEffect(() => {
-    fetchTodos(limit, batch);
-  }, [batch]);
-
-  if (todoError) {
-    return (
-      <Box>
-        <h1>Что-то пошло не так</h1>
-        <p>{todoError}</p>
-      </Box>
-    );
-  }
-
-  const handleChange = () => {};
-
   return (
     /** Основной контейнер страницы с тудушками */
-    <Box className="w-full h-full flex flex-col items-center bg-[#F5F5F5]">
+    <Box className="w-full h-full flex flex-col items-center">
       {/** Контейнер с кошачьими активностями */}
       <CarActivity />
 
-      <Box className="w-2/5 h-full flex flex-col items-center">
-        <h1 className="font-thin text-8xl text-[#E9D9D8] mb-4">todos</h1>
-        {/** Список тудушек */}
-        <TodoList todos={todos} handleTodoChange={handleChange} />
-
-        {/** Последний видимый элемент, который триггерит подгрузку новых элементов */}
-        <div className="h-[5px] opacity-0" ref={lastElement} />
+      <Box className="max-w-[90vw] min-w-[90vw] md:max-w-[50vw] md:min-w-[50vw] mx-10 h-full flex flex-col items-center">
+        <h1 className="font-thin text-8xl text-accentHeader mb-4">todos</h1>
+        {/** Форма работы с тудушками */}
+        <TodoForm />
+        {/** Кнопка добавления данных с JSONPlaceholder */}
+        {/* <Box className="w-full flex justify-end mt-5">
+          <button
+            className="px-2 py-1 text-sm rounded-md transition-colors duration-200 w-[120px] flex justify-center
+            dark:bg-bgDark dark:hover:bg-accentHeader bg-bg hover:bg-accentHeader"
+            onClick={() => handleManualFetch(limit, batch)}
+            disabled={isTodosLoading}
+          >
+            {isTodosLoading ? (
+              <Loader size={20} />
+            ) : (
+              <span
+                className="flex items-center justify-center gap-2
+              text-divider dark:text-dividerDark"
+              >
+                <FiPlusSquare />
+                add data
+              </span>
+            )}
+          </button>
+        </Box> */}
       </Box>
     </Box>
   );
