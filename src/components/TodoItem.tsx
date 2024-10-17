98614f6babbc1e154f52dd30ccbeacd78765290f
Медведев Ваня <ranzar2000@gmail.com>
2024-10-18 01:25:10 +0300

    Update the TodoItem component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,25 +4,32 @@ import { Todo } from "../types.ts/todo";
 
 interface TodoItemProps {
   todo: Todo;
-  handleChange: () => void;
+  handleChange: (todo: Todo) => void;
 }
 
 /**
  * Компонент отображения одного Todo
  * @param {TodoItemProps} props - props
  * @param {Todo} props.todo - Todo
- * @param {() => void} props.handleChange - функция, которая
+ * @param {(todo: Todo) => void} props.handleChange - функция, которая
  *   обрабатывает изменение состояния Todo
  * @returns {JSX.Element}
  */
 
 const TodoItem = ({ todo, handleChange }: TodoItemProps): JSX.Element => {
   return (
-    <Box className="flex items-center gap-4 w-full py-2">
-      <CustomCheckbox checked={todo.completed} onChange={handleChange} />
+    <Box className="flex items-center gap-4 w-full p-2">
+      <CustomCheckbox
+        checked={todo.completed}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          handleChange({ ...todo, completed: event.target.checked })
+        }
+      />
       <p
-        className={`text-2xl transition-all duration-300 ${
-          todo.completed ? "line-through text-gray-400" : "text-gray-800"
+        className={`text-2xl transition-all duration-300 break-words w-full ${
+          todo.completed
+            ? "line-through text-todoInactive dark:text-todoInactiveDark"
+            : "text-todoActive dark:text-todoActiveDark"
         }`}
       >
         {todo.title}
