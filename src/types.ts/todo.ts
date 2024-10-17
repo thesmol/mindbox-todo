7c0f77f51d0795a154e07302858226353a04da75
Медведев Ваня <ranzar2000@gmail.com>
2024-10-18 01:19:20 +0300

    Add theme and filter types

--- src/types.ts/todo.ts
+++ src/types.ts/todo.ts
@@ -1,7 +1,17 @@
 /** Тип todo */
 export type Todo = {
   userId: number;
-  id: number;
+  id: string;
   title: string;
   completed: boolean;
 };
+
+/** Тип фильтра */
+export enum FilterQuery {
+  ALL = "ALL",
+  DONE = "DONE",
+  ACTIVE = "ACTIVE",
+}
+
+// Определение enum для темы
+export type Theme = "light" | "dark";
