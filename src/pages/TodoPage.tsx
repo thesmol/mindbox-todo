77739e9caaed7faab7e1d94ae29dc3fb43bdf416
Медведев Ваня <ranzar2000@gmail.com>
2024-10-20 10:45:54 +0300

    Make TodoPage more responsive

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -10,31 +10,10 @@ const TodoPage = () => {
       {/** Контейнер с кошачьими активностями */}
       <CarActivity />
 
-      <Box className="max-w-[90vw] min-w-[90vw] md:max-w-[50vw] md:min-w-[50vw] mx-10 h-full flex flex-col items-center">
+      <Box className="max-w-[95%] min-w-[95%] md:max-w-[70%] md:min-w-[70%] lg:max-w-[50%] lg:min-w-[50%] mx-10 h-full flex flex-col items-center">
         <h1 className="font-thin text-8xl text-accentHeader mb-4">todos</h1>
         {/** Форма работы с тудушками */}
         <TodoForm />
-        {/** Кнопка добавления данных с JSONPlaceholder */}
-        {/* <Box className="w-full flex justify-end mt-5">
-          <button
-            className="px-2 py-1 text-sm rounded-md transition-colors duration-200 w-[120px] flex justify-center
-            dark:bg-bgDark dark:hover:bg-accentHeader bg-bg hover:bg-accentHeader"
-            onClick={() => handleManualFetch(limit, batch)}
-            disabled={isTodosLoading}
-          >
-            {isTodosLoading ? (
-              <Loader size={20} />
-            ) : (
-              <span
-                className="flex items-center justify-center gap-2
-              text-divider dark:text-dividerDark"
-              >
-                <FiPlusSquare />
-                add data
-              </span>
-            )}
-          </button>
-        </Box> */}
       </Box>
     </Box>
   );
