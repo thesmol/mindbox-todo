98246fbd2a10bcdb95b7965c22c51c1c5612a5cc
Медведев Ваня <ranzar2000@gmail.com>
2024-10-21 23:04:40 +0300

    Fix the overflow issue

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -6,12 +6,14 @@ import CarActivity from "../components/ui/CarActivity";
 const TodoPage = () => {
   return (
     /** Основной контейнер страницы с тудушками */
-    <Box className="w-full h-full flex flex-col items-center">
+    <Box className="w-full min-h-[100vh] flex flex-col items-center">
       {/** Контейнер с кошачьими активностями */}
       <CarActivity />
 
-      <Box className="max-w-[95%] min-w-[95%] md:max-w-[70%] md:min-w-[70%] lg:max-w-[50%] lg:min-w-[50%] mx-10 h-full flex flex-col items-center">
-        <h1 className="font-thin text-8xl text-accentHeader mb-4">todos</h1>
+      <Box className="max-w-full min-w-full md:max-w-[70%] md:min-w-[70%] lg:max-w-[50%] lg:min-w-[50%] mx-10 flex flex-col items-center overflow-y-auto p-[20px]">
+        <h1 className="w-full font-thin sm:text-8xl sm:mb-4 sm:mt-0 sm:text-center text-right text-accentHeader mt-8 mb-2 text-4xl">
+          todos
+        </h1>
         {/** Форма работы с тудушками */}
         <TodoForm />
       </Box>
