d243b55a2f028c55b7bf4aad08f686b3fd344451
Медведев Ваня <ranzar2000@gmail.com>
2024-10-23 21:16:50 +0300

    Setup jest with ts

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+export {};
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!**/vendor/**"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  transform: {
+    ".(ts|tsx)": "ts-jest",
+  },
+
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage",
+    "package.json",
+    "package-lock.json",
+    "reportWebVitals.ts",
+    "setupTests.ts",
+    "index.tsx",
+  ],
+};
